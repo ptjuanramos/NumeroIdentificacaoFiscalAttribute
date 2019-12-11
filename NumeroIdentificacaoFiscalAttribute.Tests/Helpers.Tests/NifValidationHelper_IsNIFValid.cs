aa46596d5940ce1853c2eb65262f8b642f8aabb7
Juan Ramos <ptjuanramos@gmail.com>
2019-12-11 11:11:02 +0000

    Unit tests over the validator helper

---
+++ NumeroIdentificacaoFiscalAttribute.Tests/Helpers.Tests/NifValidationHelper_IsNIFValid.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumeroIdentificacaoFiscalAttribute.Helpers.Tests
+{
+    [TestClass]
+    public class NifValidationHelper_IsNIFValid
+    {
+        [TestMethod]
+        public void IsNIFValid_InputNotValidNIF_ReturnFalse()
+        {
+            var result = NifValidationHelper.IsNIFValid("987654321");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsNIFValid_InputNIFLessThanNineDigits_ReturnFalse()
+        {
+            var result = NifValidationHelper.IsNIFValid("50370687");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsNIFValid_InputValidNIF_ReturnTrue()
+        {
+            var result = NifValidationHelper.IsNIFValid("503706876");
+            Assert.IsTrue(result);
+        }
+    }
+}
